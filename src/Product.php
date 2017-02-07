3d83f95785cf0ab1d812572de8f34fc90d923193
Idan Elhalwani <idan@elhalwani.com>
2017-02-07 12:23:30 +0200

    Set ProductParser to return an array of Product objects with relevant information

---
+++ src/Product.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Idane\ProductParser;
+class Product
+{
+    public $chainId;
+    public $chainName;
+    public $storeId;
+    public $name;
+    public $price;
+}
--- src/ProductParser.php
+++ src/ProductParser.php
@@ -12,7 +12,6 @@ class ProductParser
         $this->filename = $filename;
         $this->xml = file_get_contents($this->filename);
         $result = preg_match("/Price(?:Full)?(?<chainId>[0-9]{13})-(?P<storeId>[0-9]{3})/",$this->filename,$match);
-
         if(!$result)
         {
             throw new \Exception("Invalid XML file format!");
@@ -20,15 +19,28 @@ class ProductParser
 
         $this->chainId = $match['chainId'];
         $this->storeId = $match['storeId'];
-        echo "chainId: {$this->chainId} | storeId: {$this->storeId}";
 
 
     }
     public function parse()
     {
         $className = Constants::CHAINS[$this->chainId]['class'];
+        $chainName = Constants::CHAINS[$this->chainId]['friendly_name'];
         $parser = new $className($this->xml);
-        return $parser->parse($this->xml);
+        $result = $parser->parse($this->xml);
+        $products = [];
+        foreach($result[$className::PRODUCT_CONTAINER] as $entry)
+        {
+            $_product = new Product();
+            $_product->storeId = $this->storeId;
+            $_product->chainId = $this->chainId;
+            $_product->chainName = $chainName;
+            $_product->name = $entry['ItemName'];
+            $_product->price = $entry['ItemPrice'];
+            $products[] = $_product;
+
+        }
+        return $products;
 
     }
 }
